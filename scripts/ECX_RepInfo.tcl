03c2e6d18faa0a4af71110590e7567ef1af58fdc
y-oga <47235712+y-oga@users.noreply.github.com>
2019-07-26 15:51:29 +0900

    Support multiple mirror disks

--- scripts/ECX_RepInfo.tcl
+++ scripts/ECX_RepInfo.tcl
@@ -36,7 +36,9 @@ set fip "192.168.137.30"
 set port "29003"
 
 ### Set mirror disk name of ECX
-set mdName "md1"
+### If your cluster has one md       -> set mdName {md1}
+### If your clsuter has multiple mds -> set mdName {md1 md2 ...}
+set mdName {md1 md2 md3}
 
 ### Set recovery group name of IBM Resiliency Orchestration
 set recoveryGroup "test"
@@ -47,187 +49,182 @@ set recoveryGroup "test"
 
 
 
+
+
+### Declare the number of mirror disk
+set mdNum [llength $mdName]
+
+### Declare sum of pending data on mirror disks
+set sumDataLag 0
+
+### Declare most previous mirror break time
+set mostPreviousTimeDr ""
+
+### Start processing
+for {set i 0} {$i < $mdNum} {incr i} {
+    
+    set iMdName [lindex $mdName $i]
+
 ### Set intermediate file
-set curlOutPath "scripts/ECX/$recoveryGroup/curlOut.txt"
-set commandOutPath "scripts/ECX/$recoveryGroup/commandOut.txt"
+    set curlOutPath "scripts/ECX/$recoveryGroup/curlOut.txt"
+    set commandOutPath "scripts/ECX/$recoveryGroup/commandOut.txt"
+
 ### Set MirrorBreakTime record file
-set breakTimeRecordPath "scripts/ECX/$recoveryGroup/breakTimeRecord.txt"
+    set breakTimeRecordPath "scripts/ECX/$recoveryGroup/breakTimeRecord_$iMdName.txt"
 
 ### Create breakTimeRecord.txt if it does not exist
-set recordExist [file exists $breakTimeRecordPath]
-if {$recordExist == 0} {
-    set file_ID [open $breakTimeRecordPath w]
-    puts $file_ID "unknown"
-    close $file_ID
-}
+    set recordExist [file exists $breakTimeRecordPath]
+    if {$recordExist == 0} {
+        set file_ID [open $breakTimeRecordPath w]
+        puts $file_ID "unknown"
+        close $file_ID
+    }
 
 ### Get cluster information from ECX WebManager
-set curlOut [exec curl -s http://$fip:$port/GetMirrorInfo.js?MirrorDiskName=$mdName]
-set file_ID [open $curlOutPath w] 
-puts $file_ID "$curlOut"
-close $file_ID
+    set curlOut [exec curl -s http://$fip:$port/GetMirrorInfo.js?MirrorDiskName=$iMdName]
+    set file_ID [open $curlOutPath w] 
+    puts $file_ID "$curlOut"
+    close $file_ID
 
 ### Extract MirrorBreakTime from cluster information
-set breakTime [exec grep MirrorBreakTime $curlOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$breakTime"
-close $file_ID
+    set breakTime [exec grep MirrorBreakTime $curlOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$breakTime"
+    close $file_ID
 
 ### Extract MirrorBreakTime of a primary server
-set tmpTimePr [exec awk {-F["]} {{print $2}} $commandOutPath]
+    set tmpTimePr [exec awk {-F["]} {{print $2}} $commandOutPath]
 ### Extract MirrorBreakTime of a secondary server
-set tmpTimeDr [exec awk {-F["]} {{print $4}} $commandOutPath]
+    set tmpTimeDr [exec awk {-F["]} {{print $4}} $commandOutPath]
 
 ### Extract MirrorDiskStatus from cluster information
-set diskStatus [exec grep DiffStatus $curlOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$diskStatus"
-close $file_ID
+    set diskStatus [exec grep DiffStatus $curlOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$diskStatus"
+    close $file_ID
 
 ### Extract MirrorDiskStatus
-set diskStatus [exec awk {-F[=]} {{print $2}} $commandOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$diskStatus"
-close $file_ID
-set diskPr [exec awk {-F[,]} {{print $1}} $commandOutPath]
-set diskDr [exec awk {-F[,]} {{print $2}} $commandOutPath]
+    set diskStatus [exec awk {-F[=]} {{print $2}} $commandOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$diskStatus"
+    close $file_ID
+    set diskPr [exec awk {-F[,]} {{print $1}} $commandOutPath]
+    set diskDr [exec awk {-F[,]} {{print $2}} $commandOutPath]
 
 ### Extract ActiveStatus from cluster information
-set activeStatus [exec grep ActiveStatus $curlOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$activeStatus"
-close $file_ID
+    set activeStatus [exec grep ActiveStatus $curlOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$activeStatus"
+    close $file_ID
 
 ### Extract ActiveStatus
-set activeStatus [exec awk {-F[=]} {{print $2}} $commandOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$activeStatus"
-close $file_ID
-set activePr [exec awk {-F[,]} {{print $1}} $commandOutPath]
-set activeDr [exec awk {-F[,]} {{print $2}} $commandOutPath]
+    set activeStatus [exec awk {-F[=]} {{print $2}} $commandOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$activeStatus"
+    close $file_ID
+    set activePr [exec awk {-F[,]} {{print $1}} $commandOutPath]
+    set activeDr [exec awk {-F[,]} {{print $2}} $commandOutPath]
 
 ### Extract DiffPercent from cluster information
-set diffPercent [exec grep DiffPercent $curlOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$diffPercent"
-close $file_ID
+    set diffPercent [exec grep DiffPercent $curlOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$diffPercent"
+    close $file_ID
 
 ### Extract DiffPercent of a primary server
-set tmpDiffPercentPr [exec awk {-F["]} {{print $2}} $commandOutPath]
-if {$tmpDiffPercentPr == "--"} {
-    set tmpDiffPercentPr "0"
-}
-set diffPercentPr [format "%.1f" $tmpDiffPercentPr]
+    set tmpDiffPercentPr [exec awk {-F["]} {{print $2}} $commandOutPath]
+    if {$tmpDiffPercentPr == "--"} {
+        set tmpDiffPercentPr "0"
+    }
+    set diffPercentPr [format "%.1f" $tmpDiffPercentPr]
 ### Extract DiffPercent of a secondary server
-set tmpDiffPercentDr [exec awk {-F["]} {{print $4}} $commandOutPath]
-if {$tmpDiffPercentDr == "--"} {
-    set tmpDiffPercentDr "0"
-}
-set diffPercentDr [format "%.1f" $tmpDiffPercentDr]
+    set tmpDiffPercentDr [exec awk {-F["]} {{print $4}} $commandOutPath]
+    if {$tmpDiffPercentDr == "--"} {
+        set tmpDiffPercentDr "0"
+    }
+    set diffPercentDr [format "%.1f" $tmpDiffPercentDr]
 
 ### Extract NMPSize from cluster information
-set nmpSize [exec grep NMPSize $curlOutPath]
-set file_ID [open $commandOutPath w]
-puts $file_ID "$nmpSize"
-close $file_ID
+    set nmpSize [exec grep NMPSize $curlOutPath]
+    set file_ID [open $commandOutPath w]
+    puts $file_ID "$nmpSize"
+    close $file_ID
 
 ### Extract NMPSize of a primary server
-set tmpNmpSizePr [exec awk {-F["]} {{print $2}} $commandOutPath]
-set nmpSizePr [format "%.1f" $tmpNmpSizePr]
+    set tmpNmpSizePr [exec awk {-F["]} {{print $2}} $commandOutPath]
+    set nmpSizePr [format "%.1f" $tmpNmpSizePr]
 ### Extract NMPSize of a secondary server
-set tmpNmpSizeDr [exec awk {-F["]} {{print $4}} $commandOutPath]
-set nmpSizeDr [format "%.1f" $tmpNmpSizeDr]
-
-
-### Create a debug file
-#set file_ID [open $commandOutPath w]
-#puts $file_ID "$diskPr $diskDr end"
-#close $file_ID
+    set tmpNmpSizeDr [exec awk {-F["]} {{print $4}} $commandOutPath]
+    set nmpSizeDr [format "%.1f" $tmpNmpSizeDr]
 
 ### Delete an intermediate file
-file delete -force -- "$curlOutPath"
-file delete -force -- "$commandOutPath"
+    file delete -force -- "$curlOutPath"
+    file delete -force -- "$commandOutPath"
 
 ### Get the clock time
-set systemTime [ clock seconds ]
+    set systemTime [ clock seconds ]
 ### Convert a clock time to string
-set currentTime [ clock format $systemTime -format {%Y-%m-%d %H:%M:%S} ]
+    set currentTime [ clock format $systemTime -format {%Y-%m-%d %H:%M:%S} ]
 
 ### Convert the MirrorBreakTime of a primary server to a suitable format
-if {$tmpTimePr != "--"} {
-    regsub -all "/" $tmpTimePr {-} timePr
-} else {
-    set timePr "$tmpTimePr"
-}
+    if {$tmpTimePr != "--"} {
+        regsub -all "/" $tmpTimePr {-} timePr
+    } else {
+        set timePr "$tmpTimePr"
+    }
 
 ### Convert the MirrorBreakTime of a secondary server to a suitable format
-if {$tmpTimeDr != "--"} {
-    regsub -all "/" $tmpTimeDr {-} timeDr
-} else {
-    set timeDr "$tmpTimeDr"
-}
+    if {$tmpTimeDr != "--"} {
+        regsub -all "/" $tmpTimeDr {-} timeDr
+    } else {
+        set timeDr "$tmpTimeDr"
+    }
 
-set msg "Unknown"
-set dataLag "Unknown"
+    set msg "Unknown"
+    set dataLag "Unknown"
 
-if {$diskPr == "2" || $diskDr == "2"} {
-    set msg "Mirror disks on both servers are under Mirror Recovery. Please wait until Mirror Recovery is completed."
+    if {$diskPr == "2" || $diskDr == "2"} {
+        set msg "Mirror disks on both servers are under Mirror Recovery. Please wait until Mirror Recovery is completed."
 
-    set timePr "$currentTime"
-    set file_ID [open $breakTimeRecordPath r]
-    gets $file_ID breakTimeRecord
-    close $file_ID
-    set timeDr "$breakTimeRecord"
-} elseif {$diskPr == "8"} {
-    set msg "Primary server is not running."
-   
-    set timePr "$currentTime" 
-    set file_ID [open $breakTimeRecordPath r]
-    gets $file_ID breakTimeRecord
-    close $file_ID
-    set timeDr "$breakTimeRecord"
-} elseif {$diskDr == "8"} {
-    set msg "Secondary server is not running."
-   
-    set timePr "$currentTime" 
-    set file_ID [open $breakTimeRecordPath r]
-    gets $file_ID breakTimeRecord
-    close $file_ID
-    set timeDr "$breakTimeRecord"
-} elseif {$diskPr == "4" && $diskDr != "4"} {
-    set msg "Mirroring is not working. The disk on primary server is not up-to-date."
-    if {$timePr == "--"} {
-        set file_ID [open $breakTimeRecordPath r]
-        gets $file_ID breakTimeRecord
-        close $file_ID
-        set timePr "$breakTimeRecord"
-    }    
-    set timeDr "$timePr"
-    set timePr "$currentTime"
-    set dataLag [expr $diffPercentDr * $nmpSizeDr * 0.01]
-
-    set file_ID [open $breakTimeRecordPath w]
-    puts $file_ID "$timeDr"
-    close $file_ID
-} elseif {$diskPr != "4" && $diskDr == "4"} {
-    set msg "Mirroring is not working. The disk on secondary server is not up-to-date."
-
-    set timePr "$currentTime"
-    if {$timeDr == "--"} {
+        set timePr "$currentTime"
         set file_ID [open $breakTimeRecordPath r]
         gets $file_ID breakTimeRecord
         close $file_ID
         set timeDr "$breakTimeRecord"
-    }
-    set dataLag [expr $diffPercentPr * $nmpSizePr * 0.01]
+    } elseif {$diskPr == "8"} {
+        set msg "Primary server is not running."
+   
+        set timePr "$currentTime" 
+        set file_ID [open $breakTimeRecordPath r]
+        gets $file_ID breakTimeRecord
+        close $file_ID
+        set timeDr "$breakTimeRecord"
+    } elseif {$diskDr == "8"} {
+        set msg "Secondary server is not running."
+   
+        set timePr "$currentTime" 
+        set file_ID [open $breakTimeRecordPath r]
+        gets $file_ID breakTimeRecord
+        close $file_ID
+        set timeDr "$breakTimeRecord"
+    } elseif {$diskPr == "4" && $diskDr != "4"} {
+        set msg "Mirroring is not working. The disk on primary server is not up-to-date."
+        if {$timePr == "--"} {
+            set file_ID [open $breakTimeRecordPath r]
+            gets $file_ID breakTimeRecord
+            close $file_ID
+            set timePr "$breakTimeRecord"
+        }    
+        set timeDr "$timePr"
+        set timePr "$currentTime"
+        set dataLag [expr $diffPercentDr * $nmpSizeDr * 0.01]
 
-    set file_ID [open $breakTimeRecordPath w]
-    puts $file_ID "$timeDr"
-    close $file_ID
-} elseif {$diskPr == "4" && $diskDr == "4"} {
-    set msg "The disks on both servers are not running."
+        set file_ID [open $breakTimeRecordPath w]
+        puts $file_ID "$timeDr"
+        close $file_ID
+    } elseif {$diskPr != "4" && $diskDr == "4"} {
+        set msg "Mirroring is not working. The disk on secondary server is not up-to-date."
 
-    if {$activePr == "1"} {
         set timePr "$currentTime"
         if {$timeDr == "--"} {
             set file_ID [open $breakTimeRecordPath r]
@@ -235,34 +232,65 @@ if {$diskPr == "2" || $diskDr == "2"} {
             close $file_ID
             set timeDr "$breakTimeRecord"
         }
-
         set dataLag [expr $diffPercentPr * $nmpSizePr * 0.01]
-    } else {
-        if {$timePr == "--"} {
-            set file_ID [open $breakTimeRecordPath r]
-            gets $file_ID breakTimeRecord
-            close $file_ID
-            set timeDr "$breakTimeRecord"
-        } else{
-            set timeDr "$timePr"
+
+        set file_ID [open $breakTimeRecordPath w]
+        puts $file_ID "$timeDr"
+        close $file_ID
+    } elseif {$diskPr == "4" && $diskDr == "4"} {
+        set msg "The disks on both servers are not running."
+
+        if {$activePr == "1"} {
+            set timePr "$currentTime"
+            if {$timeDr == "--"} {
+                set file_ID [open $breakTimeRecordPath r]
+                gets $file_ID breakTimeRecord
+                close $file_ID
+                set timeDr "$breakTimeRecord"
+            }
+
+            set dataLag [expr $diffPercentPr * $nmpSizePr * 0.01]
+        } else {
+            if {$timePr == "--"} {
+                set file_ID [open $breakTimeRecordPath r]
+                gets $file_ID breakTimeRecord
+                close $file_ID
+                set timeDr "$breakTimeRecord"
+            } else{
+                set timeDr "$timePr"
+            }
+            set timePr "$currentTime"
+            set dataLag [expr $diffPercentDr * $nmpSizeDr * 0.01]
         }
+
+        set file_ID [open $breakTimeRecordPath w]
+        puts $file_ID "$timeDr"
+        close $file_ID
+    } elseif {$diskPr == "1" && $diskDr == "1"} {
+        set msg "Mirror disks on both servers are working fine."
+    
         set timePr "$currentTime"
-        set dataLag [expr $diffPercentDr * $nmpSizeDr * 0.01]
+        set timeDr "$currentTime"
+        set dataLag "0"
+
+        set file_ID [open $breakTimeRecordPath w]
+        puts $file_ID "$timeDr"
+        close $file_ID
     }
 
-    set file_ID [open $breakTimeRecordPath w]
-    puts $file_ID "$timeDr"
-    close $file_ID
-} elseif {$diskPr == "1" && $diskDr == "1"} {
-    set msg "Mirror disks on both servers are working fine."
+    set sumDataLag [expr $sumDataLag + $dataLag]
     
-    set timePr "$currentTime"
-    set timeDr "$currentTime"
-    set dataLag "0"
-
-    set file_ID [open $breakTimeRecordPath w]
-    puts $file_ID "$timeDr"
-    close $file_ID
+    if {$mostPreviousTimeDr == ""} {
+        set mostPreviousTimeDr "$timeDr"
+    } else {
+	set tmpMostPreviousTimeDr [clock scan $mostPreviousTimeDr]
+	set tmpTimeDr [clock scan $timeDr]
+	
+	if {$tmpTimeDr < $tmpMostPreviousTimeDr} {
+	    set mostPreviousTimeDr "$timeDr"
+        }
+	
+    }
 }
 
 $PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_EXIT_STATUS" "0"
@@ -270,12 +298,12 @@ $PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_OUTPUT" "$msg"
 $PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_OUTPUT_TYPE" "Text"
 
 ### Set remaining data lag
-$PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_DATALAG" "$dataLag"
+$PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_DATALAG" "$sumDataLag"
 $PANACES_CLI_RETVAL setArgs "REPLICATION_DETAILS_DATALAG_UNIT" "MB"
 
 ### Send MirrorBreakTime of both servers to RO server
 ### RPO = PR_TIMESTAMP - DR_TIMESTAMP
-$PANACES_CLI_RETVAL setArgs "DR_TIMESTAMP" "$timeDr"
+$PANACES_CLI_RETVAL setArgs "DR_TIMESTAMP" "$mostPreviousTimeDr"
 $PANACES_CLI_RETVAL setArgs "TIMESTAMP_FORMAT" "yyyy-MM-dd HH:mm:ss"
 $PANACES_CLI_RETVAL setArgs "PR_TIMESTAMP" "$timePr"
 
